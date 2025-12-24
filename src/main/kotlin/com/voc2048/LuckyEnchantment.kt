8082b369c7b79fb59fd6f0ec9a9a8345b2fbcd66
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2025-12-24 10:18:17 +0800

    Initial commit

---
+++ src/main/kotlin/com/voc2048/LuckyEnchantment.kt
@@ -0,0 +1,14 @@
+package com.voc2048
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LuckyEnchantment : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
